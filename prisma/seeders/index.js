0b469d5f53f2f91d9908d696be42e42d1282ec2f
iman mustopa kamal <mustofa56@gmail.com>
2024-01-03 17:12:10 +0700

    seeders

--- prisma/seeders/index.js
+++ prisma/seeders/index.js
@@ -1,18 +1,28 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-const { threat_type } = require('./threat-types');
-const { threat } = require('./threats');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { threat_type } = require("./threat-types");
+const { threat } = require("./threats");
+const {
+  statusStafCondition,
+  status_app_testing_period,
+  status_indirect_unit,
+  reff_currency,
+} = require("./references");
 
 async function main() {
   await threat_type();
   await threat();
+  await statusStafCondition();
+  await status_app_testing_period();
+  await status_indirect_unit();
+  await reff_currency();
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
